06cdef5844f1d04e3da12aec065e138229fbbb99
Nuno Jesus <nunomiguel533@gmail.com>
2024-02-29 23:17:07 +0000

    Code + files: replaced old lemming sprites with new temporary ones. Added collision and gravity physics to the lemmings.

--- pp/player.gd
+++ pp/player.gd
@@ -1,12 +1,38 @@
-extends RigidBody2D
+extends CharacterBody2D
+
+enum Direction { LEFT = -1, RIGHT = 1 }
+
+var screen_size
+var direction = Direction.RIGHT
+
+@export var gravity = 1
+@export var speed = 70
+
+func _ready():
+	screen_size = get_viewport_rect().size
+	velocity.x = speed
+	$AnimatedSprite2D.play()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-# Check if the player has been clicked on
-func _on_RigidBody2D_input_event( viewport: Node, event: InputEvent, shape_idx: int ):
-	if (event is InputEventMouseButton && event.pressed):
-		print("Clicked")
+func _physics_process(delta):
+	velocity.y += gravity
+	velocity.x = direction * speed
+	
+	if position.x > screen_size.x:
+		direction = Direction.LEFT
+		$AnimatedSprite2D.flip_h = true
+	if position.x < 0:
+		direction = Direction.RIGHT
+		$AnimatedSprite2D.flip_h = false
+		
+	move_and_slide()
+
+## Check if the player has been clicked on
+#func _on_RigidBody2D_input_event( viewport: Node, event: InputEvent, shape_idx: int ):
+	#if (event is InputEventMouseButton && event.pressed):
+		#print("Clicked")
 	
 
