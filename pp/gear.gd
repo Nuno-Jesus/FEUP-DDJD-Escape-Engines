e8f0842a842de37f3cd00ead4b5f2eb73c3c8024
Carlos Verissimo <carlosverissimo3001@gmail.com>
2024-03-03 16:46:21 +0000

    Collisions working for platform and gear, working on triggering the rotation

--- pp/gear.gd
+++ pp/gear.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# $AnimationPlayer.play("rotate")
 	pass # Replace with function body.
 
 # Called when the player is on top of the "button" that is going to rotate the platform
@@ -12,7 +13,7 @@ func _rotate():
 # Called when the signal "area_exited (or similar)" is emitted from the "button" that is going to rotate the platform
 # todo: connect the signal from the button to this function
 func _stopRotation():
-	$AnimationPlayer.stop()
+	$AnimationPlayer.stop("rotate")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
