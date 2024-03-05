bb06e446ec16cef3be316b9dfbe87a12e58b2b02
Carlos Verissimo <carlosverissimo3001@gmail.com>
2024-03-05 22:41:36 +0000

    Added player count and crossed count on HUD

--- pp/scripts/hud.gd
+++ pp/scripts/hud.gd
@@ -5,13 +5,18 @@ var curr_player_count_node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$PlayerCount/Count.text = str(currPlayerCount)
+	$PlayerCount/InScene.text =  "00"
 	curr_player_count_node = get_node("PlayerCount").get_node("Count")
 
 func _updatePlayers(playerCount):
 	currPlayerCount = str(playerCount)
-	$PlayerCount/Count.text = currPlayerCount
-	
+
+	# add leading 0s if less than 10 players
+	if playerCount < 10:
+		currPlayerCount = "0" + currPlayerCount
+
+	$PlayerCount/InScene.text = currPlayerCount
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
