3a08b672c3912482c3e4356e60e540809cef9112
Nuno Jesus <nunomiguel533@gmail.com>
2024-03-08 19:04:32 +0000

    Code: made a lifting animation whenever a door is fixed.

--- pp/scripts/eletric_door.gd
+++ pp/scripts/eletric_door.gd
@@ -2,13 +2,14 @@ extends Node2D
 
 var fixer_name: String = ""
 var isFixed: bool = false
+var isBeingFixed: bool = false
 
 func _ready():
 	Signals.connect("eletric_door_is_being_fixed", _on_fix)
 
 
 func _on_area_2d_body_entered(body):
-	if body.name.left(8) != "Engineer" or isFixed:
+	if body.name.left(8) != "Engineer" or isFixed or isBeingFixed:
 		return
 	fixer_name = body.name
 	Signals.emit_signal("eletric_door_spotted_engineer", fixer_name, self.name)
@@ -17,6 +18,7 @@ func _on_area_2d_body_entered(body):
 func _on_fix(name):
 	if name != self.name:
 		return
+	isBeingFixed = true
 	print("%s is being fixed!" % name)
 	$Timer.start()
 	
@@ -24,5 +26,5 @@ func _on_fix(name):
 func _on_timer_timeout():
 	print("%s is fixed!" % self.name)
 	isFixed = true
-	hide()
+	$AnimationPlayer.play("lifting")
 	Signals.emit_signal("eletric_door_is_fixed", fixer_name)
