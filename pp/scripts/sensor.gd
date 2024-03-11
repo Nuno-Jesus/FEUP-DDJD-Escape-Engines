3623def3f473d79eba8a608cd5f347590f4a2c33
Carlos Verissimo <carlosverissimo3001@gmail.com>
2024-03-11 17:32:08 +0000

    Added sensor for platform

---
+++ pp/scripts/sensor.gd
@@ -0,0 +1,23 @@
+extends Node
+
+@export var neededPlayers = 8
+
+var playersCrossed = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Label.text = str(neededPlayers)
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_area_2d_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+	if body.is_in_group("Player"):
+		playersCrossed+=1
+		
+	$Label.text = str(neededPlayers - playersCrossed)
+	
+	print(playersCrossed, " have crossed the sensor")
+	
