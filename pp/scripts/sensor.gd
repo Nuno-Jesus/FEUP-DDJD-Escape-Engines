31d420a44017d8313817b903c01271f1165d0620
Nuno Jesus <nunomiguel533@gmail.com>
2024-03-12 13:06:34 +0000

    Code + fix: added a gate to close after the sensor spotted N players. Fixed an off-by-one error that allowed 1 more player cross the sensor.

--- pp/scripts/sensor.gd
+++ pp/scripts/sensor.gd
@@ -4,21 +4,17 @@ extends Node2D
 
 var playersCrossed = 0
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	$Label.text = str(neededPlayers)
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 
 func _on_area_2d_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
 	if body.is_in_group("Player"):
-		playersCrossed+=1
+		playersCrossed += 1
 	
-	if (neededPlayers - playersCrossed) < 0:
-		$Label.text = "0"	
+	if (neededPlayers - playersCrossed) <= 0:
+		$Label.text = "0"
+		$Gate/Path2D/AnimationPlayer.play("close")
 	else:
 		$Label.text = str(neededPlayers - playersCrossed)
 	
