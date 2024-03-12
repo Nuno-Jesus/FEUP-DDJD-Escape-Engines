eee9383de70bf7946674f98610867937286b3ffa
Carlos Verissimo <carlosverissimo3001@gmail.com>
2024-03-12 21:03:50 +0000

    Preparing for merging

--- pp/scenes/camera2D.gd
+++ pp/scenes/camera2D.gd
@@ -1,13 +1,15 @@
 extends Camera2D
 
+
 var zoom_min = Vector2(0.8001, 0.8001)
 var zoom_max = Vector2(2 ,2)
 var zoom_speed = Vector2(.2, .2)
 var des_zoom = zoom
 
-func _process(delta):
+func _process(_delta):
 	zoom = lerp(zoom, des_zoom, .2)
- 
+	
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _input(event):
 	if event is InputEventMouseButton:
@@ -20,4 +22,3 @@ func _input(event):
 			if event.button_index == MOUSE_BUTTON_WHEEL_UP:
 				if zoom < zoom_max:
 					des_zoom += zoom_speed
-
