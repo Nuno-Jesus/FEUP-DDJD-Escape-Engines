ec278e55eeefc51eec4f65dfc8ec7c317c04da2a
Carlos Verissimo <carlosverissimo3001@gmail.com>
2024-03-13 19:39:44 +0000

    Reverting to old camera

---
+++ pp/scripts/camera2D.gd
@@ -0,0 +1,24 @@
+extends Camera2D
+
+
+var zoom_min = Vector2(0.8001, 0.8001)
+var zoom_max = Vector2(2 ,2)
+var zoom_speed = Vector2(.2, .2)
+var des_zoom = zoom
+
+func _process(_delta):
+	zoom = lerp(zoom, des_zoom, .2)
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			print("Zoom: ", zoom)
+			if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+				if zoom > zoom_min:
+					if ((des_zoom - zoom_speed) > zoom_min):
+						des_zoom -= zoom_speed
+			if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+				if zoom < zoom_max:
+					des_zoom += zoom_speed
