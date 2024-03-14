e96d8fc26a68342061d1113f194582fe05b1aec8
Carlos Verissimo <carlosverissimo3001@gmail.com>
2024-03-14 18:07:31 +0000

    Restart working as intended

--- pp/scripts/main.gd
+++ pp/scripts/main.gd
@@ -21,14 +21,28 @@ func _start_game():
 	$StartButton/Label.visible = false
 	$StartButton/DecorationSprites.visible = false
 	$ColorRect.visible = false
-		
-	$HUD/Time/Timer.start()
-	$Level/Spawner.start()
+	
+	get_node("HUD/Time/Timer").start()
+	get_node("Level/Spawner").start()
+	
 	
 func _on_area_2d_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton&&event.pressed:
 		if isNewGameInstance:
-			get_tree().reload_current_scene()
+			#get_tree().reload_current_scene()
+			# remove old nodes
+			get_node("HUD").free()
+			get_node("Level").free()
+			
+			var new_hud = hud_scene.instantiate()
+			new_hud.name = "HUD"
+			var new_lvl = level_scene.instantiate()
+			new_lvl.name = "Level"
+			
+			# add new ones
+			add_child(new_hud, true)
+			add_child(new_lvl, true)
+			
 		
 		_start_game()
 
@@ -41,3 +55,5 @@ func _on_end_game():
 	$StartButton/DecorationSprites.visible = true
 	$StartButton/Label.text = "Restart Game"
 	$ColorRect.visible = true
+	
+	$HUD/Time/Timer.stop()
